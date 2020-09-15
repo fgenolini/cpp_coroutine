7b63b3225c9685561c99d4b08a418fd6c5b20375
François Génolini <2612722+fgenolini@users.noreply.github.com>
2020-09-15 23:01:58 +0100

    cleanup
    minor refactoring for clarity

--- population.cpp
+++ population.cpp
@@ -1,41 +1,45 @@
-// Copied and adapted from YouTube presentation by ContextFree on
+// Copied and adapted from the YouTube presentation by ContextFree on
 // https://www.youtube.com/watch?v=fkbocd47xDE
+// (Demo: C++20 Generator Coroutines)
 // Video dated 2020/04/20
-// titled: Demo: C++20 Generator Coroutines
 
 #include <cstdint>
 #include <fstream>
+#include <iostream>
 #include <string>
-#include <vector>
 
 #include "read_rows.h"
 
 namespace frank::coro {
 
-// Expect argument 1 (second argument) to be a copy of the text file
-// obtained by unzipping
+// Expect argv[1] to be a copy of the text file obtained by unzipping
 // http://download.geonames.org/export/dump/cities500.zip
 // into cities500.txt
 void population(int argc, char *argv[]) {
-  using Population = std::int64_t;
+  constexpr auto FIELD_LATITUDE = 4;
+  constexpr auto FIELD_POPULATION = 14;
+
+  if (argc < 2 || !argv)
+    return;
+
+  using population_count = std::int64_t;
   auto args = std::vector<char *>(argv, argv + argc);
   std::string file_name{args.at(1)};
-  std::cerr << "File: " << file_name << '\n';
+  std::cout << "File: " << file_name << '\n';
   auto file = std::ifstream{file_name};
-
-  auto population_south = Population{0};
-  auto population_total = Population{0};
+  auto population_south = population_count{0};
+  auto population_total = population_count{0};
   auto row_count = 0;
   for (auto &row : read_rows(file)) {
     ++row_count;
-    if (row.size() < 4)
+    if (row.size() < FIELD_LATITUDE)
       continue;
 
-    auto latitude_text = row.at(4);
-    if (row.size() < 14)
+    auto latitude_text = row.at(FIELD_LATITUDE);
+    if (row.size() < FIELD_POPULATION)
       continue;
 
-    auto population_text = row.at(14);
+    auto population_text = row.at(FIELD_POPULATION);
     auto latitude = std::stod(std::string(latitude_text));
     auto population = std::stod(std::string(population_text));
     population_total += population;
