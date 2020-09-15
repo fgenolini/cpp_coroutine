4f2ca310987a312674a8b3d9e24ba6fa7e4722d1
François Génolini <2612722+fgenolini@users.noreply.github.com>
2020-09-15 23:36:05 +0100

    population returns population_count
    Refactor the population function to return the south population count.
    Add value of south population as of 2020/09/15

--- population.h
+++ population.h
@@ -1,7 +1,13 @@
 #pragma once
 
+#include <cstdint>
+
 namespace frank::coro {
 
-extern void population(int argc, char *argv[]);
+using population_count = std::int64_t;
 
-}
+constexpr population_count EXPECTED_SOUTH_POPULATION = 422683723;
+
+extern auto population(int argc, char const *argv[]) -> population_count;
+
+} // namespace frank::coro
