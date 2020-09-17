7ce284212f0d73a01815bcf39c57ffd624eed055
François Génolini <2612722+fgenolini@users.noreply.github.com>
2020-09-17 18:07:41 +0100

    use find_coroutine.h

--- simple_generator.cpp
+++ simple_generator.cpp
@@ -1,15 +1,8 @@
 #include "config.h"
 
+#include "find_coroutine.h"
+
 WARNINGS_OFF
-// Coroutine detection copied and adapted from
-// https://github.com/luncliff/coroutine/blob/master/interface/coroutine/channel.hpp
-#if __has_include(<coroutine>) && !defined(USE_EXPERIMENTAL_COROUTINE)
-#include <coroutine>
-namespace std_coro = std;
-#elif __has_include(<experimental/coroutine>)
-#include <experimental/coroutine>
-namespace std_coro = std::experimental;
-#endif
 #include <iostream>
 WARNINGS_ON
 
